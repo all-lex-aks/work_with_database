5b3b3e345b0278367213ea6f21c115001071f01a
all-lex-aks <alexei.k.aksyutin@gmail.com>
2026-02-03 11:56:54 +0300

    Update models.py

--- phones/models.py
+++ phones/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
+    
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     price = models.IntegerField()
