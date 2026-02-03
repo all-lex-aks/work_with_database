e6e8338c43f91df30688d00f5af64168adf2da14
all-lex-aks <alexei.k.aksyutin@gmail.com>
2026-02-03 23:05:12 +0300

    Homework done

--- phones/management/commands/import_phones.py
+++ phones/management/commands/import_phones.py
@@ -2,7 +2,7 @@ import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
-
+from django.utils.text import slugify
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -13,5 +13,13 @@ class Command(BaseCommand):
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            Phone.objects.create(
+                id=phone['id'],
+                name=phone['name'],
+                price=phone['price'],
+                image=phone['image'],
+                release_date=phone['release_date'],
+                lte_exists=phone['lte_exists'],
+                slug=slugify(phone['name'], allow_unicode=True)
+            )
+            
--- phones/views.py
+++ phones/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +9,23 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    if request.GET.get('sort') == 'name':
+        phone = Phone.objects.all().order_by('name')
+    elif request.GET.get('sort') == 'min_price':
+        phone = Phone.objects.all().order_by('price')
+    elif request.GET.get('sort') == 'max_price':
+        phone = Phone.objects.all().order_by('-price')
+    else:
+        phone = Phone.objects.all().order_by('name')
+    context = {
+        'phones': phone,
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    context = {
+        'phone': Phone.objects.get(slug=slug)
+    }
     return render(request, template, context)
